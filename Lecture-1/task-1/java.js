59bd93d53583a3d056255f4446cb7a605164a78c
miriani09 <miriani0996@gmail.com>
2020-09-25 19:50:53 +0400

    task-1

---
+++ Lecture-1/task-1/java.js
@@ -0,0 +1,22 @@
+function text(){
+    var txt = "<u>JavaScript</u>"
+    document.write(txt.italics())
+
+ }
+
+ function sum(){
+    document.write(15+15)
+ }
+
+function task3(){
+    var text = "Hello World!"
+    document.write(text.fontsize(15))    
+}
+
+function task4(){
+
+}
+
+
+
+
